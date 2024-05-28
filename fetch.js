0bb6c72283c686c6fd11588705be176bb2a9f669
dcrnek1 <dario.crnek@gmail.com>
2024-05-28 19:11:11 +0200

    final commit

--- fetch.js
+++ fetch.js
@@ -6,40 +6,42 @@ const API_KEY = "f1053c4cf59e4d2783b143837242605";
 let weatherDataStore = {};
 
 async function fetchWeather(q) {
-    try {
-        ui.loader.style.display = 'flex';
-        const response = await fetch(
-            `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${q}&days=7&aqi=yes&alerts=yes`,
-            { mode: "cors" }
-          );
-        
-          const weatherData = await response.json();
+  ui.loader.style.display = "flex";
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${q}&days=7&aqi=yes&alerts=yes`,
+      { mode: "cors" }
+    );
 
-          if(weatherData.error === undefined) {
-            weatherDataStore = weatherData;
-            updateUi(weatherDataStore);
-          } else {
-            alert("Error: " + weatherData.error.message);
-          }
-        ui.loader.style.display = 'none';
-    } catch (error) {
-        console.log(error);
+    const weatherData = await response.json();
+
+    if (weatherData.error === undefined) {
+      weatherDataStore = weatherData;
+      updateUi(weatherDataStore);
+    } else {
+      alert("Error: " + weatherData.error.message);
     }
+    ui.loader.style.display = "none";
+  } catch (error) {
+    console.log(error);
+    ui.loader.style.display = "none";
   }
-
-function fetchByLocation () {
-    navigator.geolocation.getCurrentPosition(locationSucces, locationError);
-
-        function locationSucces (location) {
-            const lat = location.coords.latitude;
-            const lon = location.coords.longitude;
-
-            fetchWeather(`${lat},${lon}`);
-        }
-    
-        function locationError () {
-            alert("No permission for location. Search for location by input!");
-        }
 }
 
-  export {fetchWeather, weatherDataStore, fetchByLocation}
+function fetchByLocation() {
+  ui.loader.style.display = "flex";
+  navigator.geolocation.getCurrentPosition(locationSucces, locationError);
+
+  function locationSucces(location) {
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+
+    fetchWeather(`${lat},${lon}`);
+  }
+  ui.loader.style.display = "none";
+  function locationError() {
+    alert("No permission for location. Search for location by input!");
+  }
+}
+
+export { fetchWeather, weatherDataStore, fetchByLocation };
--- ui.js
+++ ui.js
@@ -1,59 +1,122 @@
 const ui = {
-    gpsLocate: document.querySelector('#gps_locate'),
-    loader: document.querySelector('#loader'),
-    input: document.querySelector('input'),
-    changeC: document.querySelector('#change_c'),
-    changeF: document.querySelector('#change_f'),
-    cityName: document.querySelector('#cityName'),
-    temp: document.querySelector('#temp'),
-    tempUnit: document.querySelector('#temp_unit'),
-    currentImg: document.querySelector('#currentImg'),
-    currentDay: document.querySelector("#current_day"),
-    currentTime: document.querySelector("#current_time"),
-    condition: document.querySelector("#condition"),
-    humidity: document.querySelector("#humidity"),
-    feels_like: document.querySelector("#feels_like"),
-}
+  gpsLocate: document.querySelector("#gps_locate"),
+  loader: document.querySelector("#loader"),
+  input: document.querySelector("input"),
+  changeC: document.querySelector("#change_c"),
+  changeF: document.querySelector("#change_f"),
+  cityName: document.querySelector("#cityName"),
+  temp: document.querySelector("#temp"),
+  tempUnit: document.querySelector("#temp_unit"),
+  currentImg: document.querySelector("#currentImg"),
+  currentDay: document.querySelector("#current_day"),
+  currentTime: document.querySelector("#current_time"),
+  condition: document.querySelector("#condition"),
+  humidity: document.querySelector("#humidity"),
+  feels_like: document.querySelector("#feels_like"),
+  forecastCards: document.querySelector("#forecastCards"),
+  uvIndex: document.querySelector("#uv-index"),
+  wind: document.querySelector("#wind"),
+  windUnit: document.querySelector("#wind_unit"),
+  windDir: document.querySelector("#wind-dir"),
+  sunrise: document.querySelector("#sunrise"),
+  sunset: document.querySelector("#sunset"),
+  humidityCard: document.querySelector("#humidity_card"),
+  visibility: document.querySelector("#visibility"),
+  visibilityUnit: document.querySelector("#visibility-unit"),
+};
 
-function updateUi (weatherData, unit = ui.changeC.classList.contains('active') ? 'c' : 'f') {
-    const location = weatherData.location;
-    const forecast = weatherData.forecast;
-    const current = weatherData.current;
+function updateUi(
+  weatherData,
+  unit = ui.changeC.classList.contains("active") ? "c" : "f"
+) {
+  const location = weatherData.location;
+  const forecast = weatherData.forecast.forecastday;
+  const current = weatherData.current;
 
-    console.log(location);
-    console.log(forecast);
-    console.log(current);
+  console.log(location);
+  console.log(forecast);
+  console.log(current);
 
-    ui.cityName.textContent = location.name;
-    ui.currentImg.src = `./assets/images/weather${getImagePath(current.condition.icon)}`;
-    ui.currentDay.textContent = getDayName(current.last_updated);
-    ui.currentTime.textContent = `${(new Date(current.last_updated)).getHours()}:${(new Date(current.last_updated)).getMinutes()}`;
-    ui.condition.textContent = current.condition.text;
-    ui.humidity.textContent = current.humidity + "%";
+  //Main screen
+  ui.cityName.textContent = location.name;
+  ui.currentImg.src = `./assets/images/weather${getImagePath(
+    current.condition.icon
+  )}`;
+  ui.currentDay.textContent = getDayName(current.last_updated);
+  ui.currentTime.textContent = `${new Date(
+    current.last_updated
+  ).getHours()}:${new Date(current.last_updated).getMinutes()}`;
+  ui.condition.textContent = current.condition.text;
+  ui.humidity.textContent = `${current.humidity}%`;
+  ui.uvIndex.textContent = current.uv;
+  ui.windDir.textContent = current.wind_dir;
+  ui.sunrise.textContent = forecast[0].astro.sunrise;
+  ui.sunset.textContent = forecast[0].astro.sunset;
+  ui.humidityCard.textContent = current.humidity;
 
-    if (unit === 'c') {
-        ui.tempUnit.textContent = 'C'
-        ui.temp.textContent = current.temp_c;
-        ui.feels_like.textContent = current.feelslike_c + "°C";
-    } else {
-        ui.tempUnit.textContent = 'F'
-        ui.temp.textContent = current.temp_f;
-        ui.feels_like.textContent = current.feelslike_f + "°F";
-    }
+  //Forecast
+  console.log(forecast.length);
+  let cards = "";
+  forecast.forEach((day) => {
+    const min =
+      unit === "c" ? parseInt(day.day.mintemp_c) : parseInt(day.day.mintemp_f);
+    const max =
+      unit === "c" ? parseInt(day.day.maxtemp_c) : parseInt(day.day.maxtemp_f);
+    cards += `<div class="day-card">
+                <div class="day-card_day bold">${getDayName(day.date).substring(
+                  0,
+                  3
+                )}</div>
+                <div class="day-card_icon"><img src="${`./assets/images/weather${getImagePath(
+                  day.day.condition.icon
+                )}`}" alt="" width="55"></div>
+                <div class="day-card_temp"><span>${max}&#176; </span> <span class="grey">${min}&#176;</span>
+                </div>
+            </div>`;
+  });
+  ui.forecastCards.innerHTML = cards;
+
+  if (unit === "c") {
+    //Main screen
+    ui.tempUnit.textContent = "C";
+    ui.temp.textContent = parseInt(current.temp_c);
+    ui.feels_like.textContent = parseInt(current.feelslike_c) + "°C";
+    ui.wind.textContent = current.wind_kph;
+    ui.windUnit.textContent = "km/h";
+    ui.visibility.textContent = current.vis_km;
+    ui.visibilityUnit.textContent = "km";
+  } else {
+    //Main screen
+    ui.tempUnit.textContent = "F";
+    ui.temp.textContent = parseInt(current.temp_f);
+    ui.feels_like.textContent = parseInt(current.feelslike_f) + "°F";
+    ui.wind.textContent = current.wind_mph;
+    ui.windUnit.textContent = "m/h";
+    ui.visibility.textContent = current.vis_miles;
+    ui.visibilityUnit.textContent = "miles";
+  }
 }
 
 function getImagePath(url) {
-    let parts = url.split('/');
-    let substring = "/" + parts[parts.length - 2] + "/" + parts[parts.length - 1];
-    return substring;
+  let parts = url.split("/");
+  let substring = "/" + parts[parts.length - 2] + "/" + parts[parts.length - 1];
+  return substring;
 }
 
 function getDayName(dateString) {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let date = new Date(dateString);
-    return days[date.getDay()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = new Date(dateString);
+  return days[date.getDay()];
 }
 
 window.getDayName = getDayName;
 
-export {ui, updateUi};
+export { ui, updateUi };
