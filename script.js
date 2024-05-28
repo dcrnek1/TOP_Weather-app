65a092d7476c0b09780ae9ba18f99049403c7a1a
dcrnek1 <dario.crnek@gmail.com>
2024-05-28 19:45:37 +0200

    test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import * as listeners from "./listeners.js";
 import * as fetch from "./fetch.js";
 import { updateUi } from "./ui.js";
 
-fetch.fetchByLocation();
+fetch.fetchWeather("London");
 
 function changeUnit(unit) {
   updateUi(fetch.weatherDataStore, unit);
